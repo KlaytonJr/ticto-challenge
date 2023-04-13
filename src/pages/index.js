478d2e00198eb059003b24479cf5a28eb019d81f
KlaytonJr <klaytonjunior@protonmail.com>
2023-04-13 19:53:41 -0300

    Informations: 	- Add firebase database GET data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import styles from '@/styles/Home.module.scss'
+import firebase from '@/services/firebaseConnection'
 
 import Modal from '@/components/Modal'
 import Header from '@/components/Header'
 
 export default function Home() {
+    const [data, setData] = useState([]);
     const [modalStatus, setModalStatus] = useState(false);
+
+    const [values, setValues] = useState({
+        inValue: 0,
+        outValue: 0,
+        totalValue: 0
+    });
+
+    useEffect(() => {
+        function getData() {
+            if(!data) return;
+
+            firebase.database().ref('registers').once('value', (snapshot) => {
+                setData([]);
+
+                snapshot?.forEach((childItem) => {
+                    let data = {
+                        id: childItem.key,
+                        description: childItem.val().description,
+                        value: childItem.val().value,
+                        category: childItem.val().category,
+                        date: childItem.val().date,
+                        type: childItem.val().type
+                    }
+                    setData(oldTasks => [...oldTasks, data]);
+                })
+            })
+                .then(() => {
+                    let inValue = 0;
+                    let outValue = 0;
+                    let totalValue = 0;
+
+                    data.forEach((item) => {
+                        if(item.type === "in") inValue += +item.value;
+                        if(item.type === "out") outValue += +item.value;
+                        totalValue += +item.value;
+                    })
+
+                    setValues({
+                        inValue: inValue,
+                        outValue: outValue,
+                        totalValue: totalValue
+                    })
+                })
+        }
+
+        getData();
+    }, [data]);
 
     return (
         <>
